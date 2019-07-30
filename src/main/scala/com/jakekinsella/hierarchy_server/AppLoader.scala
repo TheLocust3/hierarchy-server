d2a75fb754c7258f4e12524826916cee9100d14e
TheLocust3 <jake.kinsella@gmail.com>
2019-07-29 20:52:57 -0400

    Classic enable cors commit

--- src/main/scala/com/jakekinsella/hierarchy_server/AppLoader.scala
+++ src/main/scala/com/jakekinsella/hierarchy_server/AppLoader.scala
@@ -1,6 +1,7 @@
 package com.jakekinsella.hierarchy_server
 
 import com.twitter.finagle.Service
+import com.twitter.finagle.http.filter.Cors
 import com.twitter.finagle.http.{Request, Response}
 import com.typesafe.config.ConfigFactory
 import io.circe.generic.auto._
@@ -11,9 +12,15 @@ import io.finch.{Application, BadRequest, InternalServerError}
 object AppLoader {
   lazy val config: HierarchyConfig = ConfigFactory.load().as[HierarchyConfig]("hierarchy").fold(throw _, identity)
 
-  lazy val api: Service[Request, Response] = routes.v1.routes.handle {
+  val policy: Cors.Policy = Cors.Policy(
+    allowsOrigin = _ => Some("*"), // TODO: will want to restrict this in the future
+    allowsMethods = _ => Some(Seq("GET", "POST", "PUT", "DELETE")),
+    allowsHeaders = _ => Some(Seq("Accept"))
+  )
+
+  lazy val api: Service[Request, Response] = new Cors.HttpFilter(policy).andThen(routes.v1.routes.handle {
     case e: io.finch.Error => BadRequest(e)
     case e: io.circe.Error => BadRequest(e)
     case t: Throwable => InternalServerError(new Exception(t.getCause))
-  }.toServiceAs[Application.Json]
+  }.toServiceAs[Application.Json])
 }
