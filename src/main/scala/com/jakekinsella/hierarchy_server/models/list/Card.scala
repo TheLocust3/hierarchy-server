aa095e82158bb1dd0f859702b6647f3278b4b263
TheLocust3 <jake.kinsella@gmail.com>
2019-09-14 23:05:24 -0400

    Send down labels and cards in list endpoint

--- src/main/scala/com/jakekinsella/hierarchy_server/models/list/Card.scala
+++ src/main/scala/com/jakekinsella/hierarchy_server/models/list/Card.scala
@@ -2,4 +2,10 @@ package com.jakekinsella.hierarchy_server.models.list
 
 import com.jakekinsella.hierarchy_server.models.graph.Data
 
-case class Card(id: String, data: Data, createdAt: Long)
+case class Card(
+  id: String,
+  data: Data,
+  createdAt: Long,
+  labels: List[Label],
+  status: Option[Status]
+)
--- src/main/scala/com/jakekinsella/hierarchy_server/service/ListService.scala
+++ src/main/scala/com/jakekinsella/hierarchy_server/service/ListService.scala
@@ -1,6 +1,7 @@
 package com.jakekinsella.hierarchy_server.service
 
-import com.jakekinsella.hierarchy_server.models.list.{Card, Column}
+import com.jakekinsella.hierarchy_server.models.graph.{Node, Tree}
+import com.jakekinsella.hierarchy_server.models.list.{Card, Column, Label, Status}
 import com.jakekinsella.hierarchy_server.store.TreeStore
 import com.twitter.util.Future
 
@@ -9,15 +10,45 @@ class ListService(treeStore: TreeStore) {
     Future {
       val tree = treeStore.matchTreeById(rootId)
       val leaves = tree.nodes.filter(tree.parent2Children.get(_) == Set.empty).toList
-      val statusTrees = tree.nodes.filter(_.data.`type` != "card").toList
 
-      statusTrees.map(statusTree => Column(
-        statusTree.id,
-        statusTree.data.title,
-        leaves
-          .filter(tree.parent2Children.get(statusTree).contains)
-          .map(node => Card(node.id, node.data, node.createdAt)),
-        statusTree.createdAt
-      ))
+      val statusTrees = tree.nodes.filter(_.data.`type` == "status").toList
+      val labelTrees = tree.nodes.filter(_.data.`type` == "label").toList
+
+      (statusTrees ++ labelTrees).map(statusTree =>
+        Column(
+          statusTree.id,
+          statusTree.data.title,
+          leaves
+            .filter(tree.parent2Children.get(statusTree).contains)
+            .map(node =>
+              Card(
+                node.id,
+                node.data,
+                node.createdAt,
+                generateLabelsForNode(node, tree, labelTrees),
+                generateStatusForNode(node, tree, statusTrees)
+              )
+            ),
+          statusTree.createdAt
+        )
+      )
     }
+
+  private def generateLabelsForNode(node: Node, tree: Tree, labelNodes: List[Node]): List[Label] = {
+    labelNodes
+      .filter(labelTree => tree.parent2Children.get(labelTree) match {
+        case Some(_) => true
+        case None => false
+      })
+      .map(node => Label(node.id, node.data.title, node.createdAt))
+  }
+
+  private def generateStatusForNode(node: Node, tree: Tree, statusNodes: List[Node]): Option[Status] = {
+    statusNodes
+      .find(status => tree.parent2Children.get(status) match {
+        case Some(_) => true
+        case None => false
+      })
+      .map(node => Status(node.id, node.data.title, node.createdAt))
+  }
 }
